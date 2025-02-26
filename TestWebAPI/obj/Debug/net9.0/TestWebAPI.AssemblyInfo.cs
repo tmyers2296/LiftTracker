661d8ef9d82c3a7344710aaca2dd8c8ee5066c5c
tmyers2296 <timmyers2296@gmail.com>
2025-02-26 17:38:09 +0000

    got rid of some of the template code from program.cs

--- TestWebAPI/obj/Debug/net9.0/TestWebAPI.AssemblyInfo.cs
+++ TestWebAPI/obj/Debug/net9.0/TestWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb8e0a3922342b13dee19683a692162b4081fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88edd8750bc3c767ad7aad757682a128ddc1a256")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
