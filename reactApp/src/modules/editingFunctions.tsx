a852fad7487779dbbec8bd7730ab860114b93448
tmyers2296 <timmyers2296@gmail.com>
2025-11-05 15:50:42 +0000

    Designing helper function for shifting order

--- reactApp/src/modules/editingFunctions.tsx
+++ reactApp/src/modules/editingFunctions.tsx
@@ -1,5 +1,25 @@
-// Shifting functions
+// **Shifting functions**
+// moves the revelant item up or down in it's list for
+// instance an exercise in a routine or a set in an exercise
+export function swapOrder<T extends { id: number; order: number }>(
+    objectList: T[],
+    targetId: number,
+    direction: "up" | "down"
+): T[] {
+    const item = objectList.find((i) => i.id === targetId);
+    if (!item) return objectList;
 
-// CRUD functions
+    const newOrder = direction === "up" ? item.order + 1 : item.order - 1;
+    const adjacent = objectList.find((i) => i.order === newOrder);
+    if (!adjacent) return objectList;
 
-// Create temp ids functions
+    return objectList.map((object) => {
+        if (object.id === targetId) return { ...object, order: newOrder };
+        if (object.id === adjacent.id) return { ...object, order: item.order };
+        return object;
+    });
+}
+
+// **CRUD functions**
+
+// **Create temp ids functions**
