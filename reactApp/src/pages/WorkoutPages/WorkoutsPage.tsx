6acd3db793deb9d28e203db10fe72d98bd5daf4e
tmyers2296 <timmyers2296@gmail.com>
2025-11-24 17:34:21 +0000

    building pages for following a routine vs improvising a workout

--- reactApp/src/pages/WorkoutPages/WorkoutsPage.tsx
+++ reactApp/src/pages/WorkoutPages/WorkoutsPage.tsx
@@ -1,12 +1,31 @@
 import { NavBar } from "../../components/Navigation/NavBar";
 import AuthorizeView from "../../components/AuthorizeView";
+import styles from "./WorkoutPages.module.css";
+import { Tooltip } from "react-tooltip";
 
 function WorkoutsPage() {
     return (
         <AuthorizeView>
             <div>
                 <NavBar />
-                ...
+                <div className={styles.itemBox}>
+                    <button
+                        className={styles.addButtonOrange}
+                        data-tooltip-id="routine-tooltip"
+                        data-tooltip-content="Record workout from routine"
+                    >
+                        +
+                    </button>
+                    <Tooltip id="routine-tooltip" className={styles.tooltip} />
+                    <button
+                        className={styles.addButtonBlue}
+                        data-tooltip-id="improv-tooltip"
+                        data-tooltip-content="Improvise workout"
+                    >
+                        +
+                    </button>
+                    <Tooltip id="improv-tooltip" className={styles.tooltip} />
+                </div>
             </div>
         </AuthorizeView>
     );
